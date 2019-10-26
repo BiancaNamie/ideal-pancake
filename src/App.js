5e2f0495b51f585b0f64c7d5b0e8454b1e94e694
BiancaNamie <biancanamie@gmail.com>
2019-10-26 15:53:10 -0300

    todoList'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,87 @@
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import List from './views/List';
 
-import Alert from "react-bootstrap/Alert";
-import React from "react";
+import React, {Component} from "react";
+import * as TodoModel from "./models/TodoModel";
 
+//import Alert from "react-bootstrap/Alert";
+//import React from "react";
+
+
+class App extends Component{
+  state = {
+    name: "",
+    list: []
+  }
+
+  componentDidMount () {
+    this.getTodos();
+  }
+
+  getTodos = () => {
+    TodoModel.getTodos()
+    .then((value)=> this.updateList(value))
+    .catch((error) => console.error(error));
+  }
+  
+  updateList = (value) =>{
+    this.setState(()=> ({
+      list: value
+    }))
+  } 
+
+  onChange = (event) =>{
+
+    const {value} = event.target;
+    this.setState(() => ({
+      name: value
+    }))
+
+   // console.log(event.target.name, "value: ", event.target.value);
+    //this.setState{() => ({
+     // name: event.target.value
+    //})
+  }
+
+  onClick = () => {
+    const {name} = this.state;
+    TodoModel.add(name)
+      .then( this.getTodos /*value => this.updateList(value)*/)
+      .catch(error => console.error(error))
+  
+    /*  this.setState(() => ({
+      list: [...list, {title : name}],
+      name:"",
+    }))*/
+  }
+
+  remove = id => {
+    TodoModel.destroy(id)
+      .then(this.getTodos)
+      .catch(error => console.error(error))
+    /*const {list} = this.state;
+    this.setState(() => ({
+      list: list.filter(item => item.id !== id),
+    }))*/
+  }
+
+  render(){
+    const{ name, list } = this.state;
+
+    return(
+      <div>
+        <input type ="text" value={name} onChange={(event) => this.onChange(event)} />
+        <button onClick={this.onClick}>Add</button>
+        <List list={list} onRemove={this.remove}/>
+      </div>
+    )
+  }
+}
+
+
+
+/**
 function App() {
   return (
     <div className="App">
@@ -13,5 +91,5 @@ function App() {
     </div>
   );
 }
-
+ */
 export default App;
---
+++ src/models/TodoModel.js
@@ -0,0 +1,18 @@
+import * as TodoRepository from "../repository/TodoRepository"
+
+export const getTodos = async () => {
+    const result = await TodoRepository.get();
+    return result;
+} 
+
+export const add = async (name) => {
+    console.log(name)
+    const newItem = {
+        title: name
+    }
+    return await TodoRepository.add(newItem)
+}
+
+export const destroy = async (id) => {
+    return await TodoRepository.destroy(id)
+}
